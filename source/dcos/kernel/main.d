549208a4e04892bc18d731c4462ad722211e7d93
Ikey Doherty <ikey@serpentos.com>
2023-05-08 16:43:37 +0100

    kernel: Begin main module
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/dcos/kernel/main.d
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2023 Ikey Doherty
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * main
+ *
+ * Kernel main entry
+ *
+ * Authors: Copyright © 2023 Ikey Doherty
+ * License: Zlib
+ */
+module main;
